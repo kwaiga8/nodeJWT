eb6c03b4a444d16984ba03f19e89905cd2140c69
utdiscas <utdiscas@protonmail.com>
2020-12-11 23:04:31 +0100

    jwt added

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ express app with JWT tutorial based on Dev Ed tutorial
 
 ## to start app:
 
-npm start
+####npm start
 
-to connect to DB .env file was created, file is not kept in version control
+##.env file was created, file is not kept in version control, connection to db and jwt token secret are stored there:
+####DB_CONNECT
+####TOKEN_SECRET
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../model/User');
 const {registerValidation, loginValidation} = require('../validation');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 //VALIDATION
@@ -48,6 +49,10 @@ router.post('/login', async (req,res) =>{
         const validPass = await bcrypt.compare(req.body.password, user.password);
         if(!validPass) return res.status(423).send('Invalid password');
     }
+    //create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+
     res.send('Logged in');
 });
 
