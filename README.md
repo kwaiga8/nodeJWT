f7e9462a22a29547365d52b6564e421b6bdf556f
utdiscas <utdiscas@protonmail.com>
2020-12-11 22:44:05 +0100

    loggin functionality added

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ express app with JWT tutorial based on Dev Ed tutorial
 ## to start app:
 
 npm start
+
+to connect to DB .env file was created, file is not kept in version control
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const {registerValidation} = require('../validation');
+const {registerValidation, loginValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
 
 
 //VALIDATION
@@ -20,18 +21,34 @@ router.post('/register', async (req, res, next) => {
         if (emailExist){
             return res.status(433).send('Email already exists');
         }
+        //Hash the passwords
+            const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
         const user = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         });
         try {
             const savedUser = await user.save();
-            res.send(savedUser);
+            res.send({user: user._id});
         } catch (err) {
             res.status(419).send(err);
         }
     }
+});
+
+//LOGIN
+router.post('/login', async (req,res) =>{
+    const { error} = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const user = await User.findOne({email: req.body.email});
+    if (!user){
+        return res.status(423).send('Email not registered');
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        if(!validPass) return res.status(423).send('Invalid password');
+    }
+    res.send('Logged in');
 });
 
 module.exports = router;
